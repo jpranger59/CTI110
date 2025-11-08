979048de643f2fed379c1877ce98e6f9cc00b13a
jpranger59 <dossd3810@student.faytechcc.edu>
2025-11-08 12:17:02 -0500

    P1HW1

---
+++ P1HW1.py
@@ -0,0 +1,22 @@
+ # Doss, Donivin
+ # 11/08/2025
+ # P1HW1
+ # Math and Integers
+
+print("------Calculating Exponets-----")
+print()
+print()
+base_value=int(input("Enter an integer as the base value:"))
+exponent=int(input("Enter an integer as the exponent:"))
+print()
+print()
+print(base_value, "raised to the power of", exponent, "is", base_value**exponent, "!!")
+print()
+print()
+print("------Addition and Subtraction-----")
+print()
+print()
+starting=int(input("Enter a starting integer:"))
+add=int(input("Enter an integer to add:"))
+sub=int(input("Enter an integer to subtract:"))
+print(starting,"+", add,"-", sub, "is equal to", starting+add-sub)
