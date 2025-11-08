feee38428928a33731da7579e11c24688c5b0795
jpranger59 <dossd3810@student.faytechcc.edu>
2025-11-08 12:43:47 -0500

    P1HW2

---
+++ P1HW2.py
@@ -0,0 +1,22 @@
+ # Doss, Donivin
+ # 11/08/2025
+ # P1HW2
+ # travel expenses
+ # program Pseudocode (logic)
+
+budget=int(input("Enter your budget:"))
+destination=input("Enter your destination:")
+gas=int(input("How much do you think you'll spend on gas?"))
+accomodations=int(input("How much will you spend on accomodations?"))
+food=int(input("How much will you spend on food?"))
+print()
+print()
+print("Travel Expenses")
+print("location:", destination)
+print("Initial Budget:", budget)
+print()
+print("Fuel:", gas)
+print("Accomodations:", accomodations)
+print("Food:", food)
+print()
+print("Remaining Balance", budget-gas-accomodations-food)
